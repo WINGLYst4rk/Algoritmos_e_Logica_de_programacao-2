2fcd7f8498feb59dd0b358a7cb0493421582ed3c
WINGLYst4rk <wingly205@gmail.com>
2020-06-16 22:19:45 -0300

    adição de um novo progrmas sobre ordenação de vetores usando bubblesort

---
+++ ordenacao_de_vetores/main.c
@@ -0,0 +1,33 @@
+//exenplo de ordenação usando blubblesort
+#include <stdio.h>
+#include <locale.h>//biblioteca para uso de acentos
+
+//metódo principal do progrma 
+int main(){
+	int vetorA[10];
+	int i, j, troca;
+	setlocale(LC_ALL,"");//uso da biblioteca locale para acento
+	
+	
+	//loop para percorrer o vetor
+	for (i=0;i<10;i++){
+		printf ("Digite o %d elemento:", i);
+		scanf ("%d", &vetorA[i]);
+		
+	}
+	for (i=0; i<9; i++){
+		for (j=i+1; j<10; j++){
+			if (vetorA[i] > vetorA[j]){
+				troca = vetorA[i];
+				vetorA[i] = vetorA[j];
+				vetorA[j] = troca;
+			}
+		}
+	}
+	printf ("\n VETOR ORDENADO: \n");
+	for (i=0; i<10; i++){
+		printf ("%d - ", vetorA[i]);
+		
+	}
+	return(0);
+}
