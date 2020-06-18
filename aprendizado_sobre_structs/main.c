bfc8c323b343a4198e6b11786a986a926490bd7a
WINGLYst4rk <wingly205@gmail.com>
2020-06-18 18:44:43 -0300

    correções no código e teste de funcionalidade

--- aprendizado_sobre_structs/main.c
+++ aprendizado_sobre_structs/main.c
@@ -1,8 +1,37 @@
+//programa extraido do livro da Unicesumar de Algoritmos e logica de programação 2
+//apenas um aprendizado sobe como os structs funcionam
+
+//declaração das bibliotecas necessarias
 #include <stdio.h>
-#include <stdlib.h>
+#include <locale.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//inicio do código
+struct produto //declaração do struct
+{
+	int codigo;
+	char descricao[50];
+	float preco;
+	int saldo;
+};
 
-int main(int argc, char *argv[]) {
-	return 0;
+int main()
+{
+	setlocale(LC_ALL,""); /*uso da biblioteca locale para permitir o acento*/
+	struct produto ficha;
+	
+	printf("Digite o código do produto:");
+	scanf("%d",&ficha.codigo);
+	printf("Digite a descrição do produto:");
+	scanf("%s", ficha.descricao);
+	printf("Digite o preço do produto:");
+	scanf("%f", &ficha.preco);
+	printf("Digite o saldo do protudo:");
+	scanf("%d", &ficha.saldo);
+	
+	printf("Código : %d\n", ficha.codigo);
+	printf("Descrição : %s\n", ficha.descricao);
+	printf("Preço : %.2f\n", ficha.preco);
+	printf("Saldo : %d\n", ficha.saldo);
+	
+	return(0);
 }
